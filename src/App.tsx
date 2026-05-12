cadc8e916516b8545db1c3ce7b447cf20e4a0428
Adam Swanson <adam.swanson.98@gmail.com>
2026-05-12 17:00:16 -0400

    Remove landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
-import { LandingPage } from "./pages/LandingPage";
 
-export const App = () => {
-  if (!import.meta.env.DEV) {
-    return <LandingPage />;
-  }
-
-  return (
-    <>
-      <Home />
-      <Footer />
-    </>
-  );
-};
+export const App = () => (
+  <>
+    <Home />
+    <Footer />
+  </>
+);
